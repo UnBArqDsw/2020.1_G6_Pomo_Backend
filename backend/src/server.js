f20a50c5e812becf35df02154c5892bfb23db432
thallysbraz <thallysbraz3@gmail.com>
2020-10-05 22:33:59 -0300

    subindo strutura inicial do projeto

---
+++ backend/src/server.js
@@ -0,0 +1,8 @@
+import app from "./app";
+
+const port = process.env.PORTA || 3001; //porta que o server esta rodando
+
+//start server NodeJS
+app.listen(port, () => {
+  console.log(`server rodando na porta: ${port}`);
+});
