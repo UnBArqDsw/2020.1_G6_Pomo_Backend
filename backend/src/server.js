e25a6656fa15730cc187e0cb06c7f9303f952c06
Arthur <arthurarp@hotmail.com>
2020-10-07 19:31:50 -0300

    [FEATURE] Adicionando docker config

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,6 @@
 import app from "./app";
 
-const port = process.env.PORTA || 3001; //porta que o server esta rodando
+const port = process.env.PORTA || 3333; //porta que o server esta rodando
 
 //start server NodeJS
 app.listen(port, () => {
