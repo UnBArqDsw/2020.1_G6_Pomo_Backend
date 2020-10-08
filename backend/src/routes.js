6c008cc53ca9b4fdb3c2b45584e593745b7c204b
Arthur <arthurarp@hotmail.com>
2020-10-08 17:44:39 -0300

    [FEATURE] UnBArqDsw/2020.1_G6_Pomo#25 Criando o arquivo migrations

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,8 +1,20 @@
 import { Router } from "express";
 
 import UserController from "./app/controllers/UserController";
+import Task from './app/models/Task';
 
 const routes = new Router();
+
+routes.get('/', async (req, res) => { // exemplo para testar se esta funcionando
+    const user = await Task.create({
+        name: 'Estudar calculo',
+        time: '2 horas',
+        icon: 'calculo',
+        color: 'red',
+    });
+
+    return res.json(user);
+});
 
 routes.post("/users", UserController.store);
 
