778bbc19eb21b9690b6b1f5f74c0379a02cc6930
thallysbraz <thallysbraz3@gmail.com>
2020-10-08 21:26:46 -0300

    criando upload de foto para perfil

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,10 +1,15 @@
 import { Router } from "express";
+import multer from "multer";
+
+import authMiddleware from "./app/middleware/auth"; //middleware de autenticação
+import multerConfig from "./config/multer"; //config multer
 
 import UserController from "./app/controllers/UserController";
 import SessionController from "./app/controllers/SessionController";
-import authMiddleware from "./app/middleware/auth"; //middleware de autenticação
+import FileController from "./app/controllers/FileController";
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post("/users", UserController.store);
 routes.post("/session", SessionController.store);
@@ -12,5 +17,6 @@ routes.post("/session", SessionController.store);
 routes.use(authMiddleware); //Middleware global || valido para rotas abaixo
 
 routes.put("/users", UserController.update);
+routes.post("/files", upload.single("file"), FileController.store);
 
 export default routes;
