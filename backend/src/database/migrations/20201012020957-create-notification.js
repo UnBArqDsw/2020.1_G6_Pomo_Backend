51f424c7bad25dd02e86de2886c5805a51221f73
Marco Antonio <markinlimac@gmail.com>
2020-10-11 23:15:53 -0300

    [FEATURE] UnBArqDsw/2020.1_G6_Pomo#24 Adicionando migrations

---
+++ backend/src/database/migrations/20201012020957-create-notification.js
@@ -0,0 +1,30 @@
+"use strict";
+
+module.exports = {
+  up: (queryInterface, Sequelize) =>
+    queryInterface.createTable("notification", {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      body: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    }),
+  down: queryInterface => queryInterface.dropTable("notification"),
+};
