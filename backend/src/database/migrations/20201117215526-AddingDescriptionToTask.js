b1ce0754afb1cf47dbc694817852042e3ae97fb9
Arthur <arthurarp@hotmail.com>
2020-11-19 02:52:50 -0300

    [BUG] Corrigindo alguns campos da migration

--- backend/src/database/migrations/20201117215526-AddingDescriptionToTask.js
+++ backend/src/database/migrations/20201117215526-AddingDescriptionToTask.js
@@ -9,6 +9,16 @@ module.exports = {
         autoIncrement: true,
         primaryKey: true,
       },
+      user_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: "users",
+          key: "id",
+        },
+        onUpdate: "CASCADE",
+        onDelete: "CASCADE",
+        allowNull: false,
+      },
       name: {
         type: Sequelize.STRING,
         allowNull: false,
